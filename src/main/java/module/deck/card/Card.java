4569fba8ae838d4790f9e94a76693d20b21c7f87
Davide Orizio <davide.orizio@mail.polimi.it>
2024-03-21 16:24:45 +0100

    Added abstract class module.deck.card.Card

--- src/main/java/module/deck/card/Card.java
+++ src/main/java/module/deck/card/Card.java
@@ -1,4 +1,41 @@
 package module.deck.card;
 
-public class Card {
+/**
+ *
+ * @author Orizio Davide
+ *
+ * A general card
+ */
+public abstract class Card {
+    private final int points;
+    private final String frontImage;
+    private final String backImage;
+
+    /**
+     *
+     * @param points: the number of points obtained playing/completing the card
+     * @param frontImage: path to the front image
+     * @param backImage: path to the back image
+     * @throws NullPointerException: when a path is null
+     * @throws IllegalArgumentException: if points is a negative number
+     */
+    public Card(int points, String frontImage, String backImage) throws NullPointerException, IllegalArgumentException {
+        if ((frontImage == null) || (backImage == null)) throw new NullPointerException("String cannot be null");
+        if (points < 0) throw new IllegalArgumentException("Points must be a positive integer");
+        this.points = points;
+        this.frontImage = frontImage;
+        this.backImage = backImage;
+    }
+
+    public int getPoints() {
+        return this.points;
+    }
+
+    public String getFrontImage() {
+        return this.frontImage;
+    }
+
+    public String getBackImage() {
+        return this.backImage;
+    }
 }
