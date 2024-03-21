96b170821f14c8744b56f2adcc932c9125f7f713
Davide Orizio <davide.orizio@mail.polimi.it>
2024-03-21 16:32:30 +0100

    Added corner enums CornerType and CornerPos in module.deck.card.playablecard.corner

--- src/main/java/module/deck/card/playablecard/corner/CornerPos.java
+++ src/main/java/module/deck/card/playablecard/corner/CornerPos.java
@@ -1,4 +1,11 @@
 package module.deck.card.playablecard.corner;
 
+/**
+ * Position of the card's corner (both back and front)
+ */
 public enum CornerPos {
+    TOPLEFT,
+    TOPRIGHT,
+    BOTTOMLEFT,
+    BOTTOMRIGHT
 }
--- src/main/java/module/deck/card/playablecard/corner/CornerType.java
+++ src/main/java/module/deck/card/playablecard/corner/CornerType.java
@@ -1,4 +1,10 @@
-package module.board.cards.playablecard.corner;
+package module.deck.card.playablecard.corner;
 
+/**
+ * Tells if the corner is visibile (playable)
+ * or hidden (not playable)
+ */
 public enum CornerType {
+    HIDDEN,
+    VISIBLE
 }
