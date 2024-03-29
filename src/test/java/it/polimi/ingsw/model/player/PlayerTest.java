862041f63263868765bd484d01836ff7d57515ec
Davide Orizio <davide.orizio@mail.polimi.it>
2024-03-29 18:39:51 +0100

    1. Added Mockito dependency to Maven 2. Created test for Player class

---
+++ src/test/java/it/polimi/ingsw/model/player/PlayerTest.java
@@ -0,0 +1,77 @@
+package it.polimi.ingsw.model.player;
+import it.polimi.ingsw.model.deck.card.playablecard.PlayableCard;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import java.util.NoSuchElementException;
+import java.util.Random;
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+    private Player p;
+
+    /**
+     * Before every test create a new player
+     */
+    @Before
+    public void playerSetUp() {
+        p = new Player("dolby", Marker.GREEN);
+    }
+
+    /**
+     * - the score value is 0 after the class creation.
+     * - the value is added correctly.
+     * - the exception is thrown when the parameter is negative.
+     */
+    @Test
+    public void increaseScoreTest() {
+        assertEquals(0, p.getScore());
+        Random r = new Random();
+        int tmp, exp;
+        int max = 10;
+        for (int i = 0; i < max; i++) {
+            tmp = r.nextInt(max);
+            exp = tmp + p.getScore();
+            p.increaseScore(tmp); // 0 < tmp < 9
+            assertEquals(exp, p.getScore());
+        }
+        for (int i = 0; i < max; i++) {
+            int t = -1 - r.nextInt(max); // -11 < t < -1
+            assertThrows(IllegalArgumentException.class, () -> p.increaseScore(t));
+        }
+    }
+
+    /**
+     * - the method adds correctly the card to the list
+     * - if the hand is already full throws an exception
+     */
+    @Test
+    public void addToHandTest() {
+        PlayableCard pc;
+        for (int i = 0; i < 3; i++) {
+            pc = Mockito.mock(PlayableCard.class);
+            assertFalse(p.getHand().contains(pc));
+            p.addToHand(pc);
+            assertTrue(p.getHand().contains(pc));
+        }
+        assertThrows(IllegalStateException.class, () -> p.addToHand(Mockito.mock(PlayableCard.class)));
+    }
+
+    /**
+     * - a card is removed correctly if present
+     * - an exception is thrown if the card is not found
+     */
+    @Test
+    public void removeFromHandTest() {
+        PlayableCard pc;
+        for (int i = 0; i < 3; i++) {
+            pc = Mockito.mock(PlayableCard.class);
+            assertFalse(p.getHand().contains(pc));
+            p.addToHand(pc);
+            assertTrue(p.getHand().contains(pc));
+            p.removeFromHand(pc);
+            assertFalse(p.getHand().contains(pc));
+        }
+        assertThrows(NoSuchElementException.class, () -> p.removeFromHand(Mockito.mock(PlayableCard.class)));
+    }
+}
