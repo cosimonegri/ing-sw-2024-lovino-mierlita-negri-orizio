e299fd611bc2ec1fada002f8dd27200c5019c206
Davide Orizio <davide.orizio@mail.polimi.it>
2024-05-18 12:30:55 +0200

    - changed background image - added text box

--- src/main/java/it/polimi/ingsw/view/GUI.java
+++ src/main/java/it/polimi/ingsw/view/GUI.java
@@ -2,12 +2,15 @@ package it.polimi.ingsw.view;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.geometry.Pos;
 import javafx.scene.*;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import javafx.scene.text.*;
 
 public class GUI extends View {
 
@@ -26,20 +29,40 @@ public class GUI extends View {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Image backgroundImage = new Image("file:/home/dolby/Downloads/arch.png");
+        String backgroundPath = "file:src/main/resources/images/background.jpg";
+        Image backgroundImage = new Image(backgroundPath);
         BackgroundImage background = new BackgroundImage(backgroundImage,
                 BackgroundRepeat.NO_REPEAT,
                 BackgroundRepeat.NO_REPEAT,
                 BackgroundPosition.CENTER,
-                new BackgroundSize(BackgroundSize.AUTO,
-                        BackgroundSize.AUTO, false, false, true, false));
-        Pane pane = new Pane();
-        pane.setBackground(new Background(background));
+                new BackgroundSize(1.0,
+                        BackgroundSize.AUTO, true, true, false, false));
+        Pane backgroundPane = new Pane();
+        backgroundPane.setBackground(new Background(background));
 
-        Scene scene = new Scene(pane, 800, 600);
+
+
+        Text text = new Text("Welcome to Code Naturalis");
+
+        text.setFont(new Font("Arial", 40));
+        text.setFill(Color.WHITE);
+
+        VBox textBox = new VBox(text);
+        textBox.setAlignment(Pos.CENTER);
+        textBox.setStyle("-fx-background-color: rgba(0,0,0,0.5); -fx-padding: 20px");
+
+        StackPane root = new StackPane();
+        root.getChildren().addAll(backgroundPane, textBox);
+        StackPane.setAlignment(textBox, Pos.CENTER);
+
+        Scene scene = new Scene(root, 800, 600);
         primaryStage.setScene(scene);
         primaryStage.setFullScreen(true);
-        primaryStage.setTitle("Arch linux");
+        primaryStage.setTitle("Code Naturalis");
+
+        StackPane.setAlignment(text, Pos.CENTER);
+
+
 
         primaryStage.show();
     }
