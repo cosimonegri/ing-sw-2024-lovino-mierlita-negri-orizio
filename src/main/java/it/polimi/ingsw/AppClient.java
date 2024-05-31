9af229fbd06c98c1879f983c4340295fb914c84b
Alexandru Cezar Mierlita <alexandrucezar.mierlita@gmail.com>
2024-05-31 13:33:58 +0200

    Fix parameters in AppClient

--- src/main/java/it/polimi/ingsw/AppClient.java
+++ src/main/java/it/polimi/ingsw/AppClient.java
@@ -8,9 +8,6 @@ import it.polimi.ingsw.view.View;
 
 public class AppClient {
     private static final String IPV4_PATTERN = "^(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\\.(?!$)|$)){4}$";
-    private View view;
-    private String connection;
-    private String ip;
 
     private void parseArgs(String[] args) {
         // parse connection
@@ -26,22 +23,40 @@ public class AppClient {
         for (int i = 0; i < args.length - 1; i++) {
             switch (args[i]) {
                 // connection type, default socket
-                case "-n" -> connection = args[i + 1].equalsIgnoreCase("RMI") ? "RMI" : null;
+                case "-n" -> {
+                    if(args[i + 1].equalsIgnoreCase("RMI")) {
+                        connection = "RMI";
+                    } else if(args[i + 1].equalsIgnoreCase("SOCKET")) {
+                        connection = "SOCKET";
+                    } else {
+                        System.err.println("Invalid connection parameter.");
+                        System.exit(1);
+                    }
+                }
                 // user interface, default gui
-                case "-i" -> view = args[i + 1].equalsIgnoreCase("TUI") ? new TUI() : null;
+                case "-i" -> {
+                    if(args[i + 1].equalsIgnoreCase("TUI")) {
+                        view = new TUI();
+                    } else if(args[i + 1].equalsIgnoreCase("GUI")) {
+                        view = new GUI();
+                    } else {
+                        System.err.println("Invalid user interface parameter.");
+                        System.exit(1);
+                    }
+                }
                 // ip, default localhost
-                case "-h" -> ip = args[i + 1].matches(IPV4_PATTERN) ? args[i + 1] : null;
+                case "-h" -> {
+                    if (args[i + 1].matches(IPV4_PATTERN)) {
+                        ip = args[i + 1];
+                    } else {
+                        System.err.println("Invalid ip parameter.");
+                        System.exit(1);
+                    }
+                }
             }
         }
-        if (connection == null || view == null || ip == null) {
-            System.err.println("Invalid parameters.");
-            System.exit(1);
-        }
-//        System.out.println("Connection: " + connection);
-//        System.out.println("View: " + view.getClass());
-//        System.out.println("Ip: " + ip);
+
         Client client = new Client(view, ConnectionType.valueOf(connection), ip);
         client.run();
-
     }
 }
