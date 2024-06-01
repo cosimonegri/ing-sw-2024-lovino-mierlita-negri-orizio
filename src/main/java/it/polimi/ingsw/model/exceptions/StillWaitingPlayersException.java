23b0ec4ee5e607f20a80c0c55c0a2542fb209b92
cosimonegri <cosimogiovanninegri@gmail.com>
2024-06-01 12:40:51 +0200

    Remove unused exceptions

--- src/main/java/it/polimi/ingsw/model/exceptions/StillWaitingPlayersException.java
+++
@@ -1,8 +0,0 @@
-package it.polimi.ingsw.model.exceptions;
-
-/**
- * Exception thrown when server tries to start game before all players have joined
- */
-public class StillWaitingPlayersException extends Exception{
-    public StillWaitingPlayersException(){ super(); }
-}
