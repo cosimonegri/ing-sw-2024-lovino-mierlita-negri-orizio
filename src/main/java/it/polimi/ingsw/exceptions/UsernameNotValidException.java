2a423255fbd5cf54cf943a92488f39620754b9a8
cosimonegri <cosimogiovanninegri@gmail.com>
2024-06-21 22:59:18 +0200

    Add max length for the username

--- src/main/java/it/polimi/ingsw/exceptions/UsernameNotValidException.java
+++ src/main/java/it/polimi/ingsw/exceptions/UsernameNotValidException.java
@@ -1,11 +1,13 @@
 package it.polimi.ingsw.exceptions;
 
+import it.polimi.ingsw.utilities.Config;
+
 /**
  * Exception thrown when the player tries to choose an empty username
  * or a username that contains invalid characters
  */
 public class UsernameNotValidException extends Exception {
     public UsernameNotValidException() {
-        super("The username can only contain alphanumeric characters or underscores, and it cannot be empty");
+        super(Config.USERNAME_NOT_VALID_MESSAGE);
     }
 }
--- src/main/java/it/polimi/ingsw/utilities/Config.java
+++ src/main/java/it/polimi/ingsw/utilities/Config.java
@@ -6,7 +6,6 @@ public class Config {
     public static final int RMI_PORT = 1099;
     public static final String RMI_NAME = "CodexNaturalisServer-PSP33";
 
-
     public static final int PING_TIME_MS = 5000;
 
     public static final int MIN_GAME_ID = 0;
@@ -14,8 +13,16 @@ public class Config {
 
     public static final int SCORE_FOR_FINAL_PHASE = 20;
 
+    public static final int MAX_USERNAME_LENGTH = 30;
+    public static final String USERNAME_NOT_VALID_MESSAGE = "The username can only contain alphanumeric characters or underscores, " +
+            "it cannot be empty and it must be less than " + MAX_USERNAME_LENGTH + " characters.";
+
     public static boolean isUsernameValid(String username) {
-        return !username.isEmpty() && !username.isBlank() && username.matches("[a-zA-Z_0-9]*");
+        return username != null
+                && !username.isEmpty()
+                && !username.isBlank()
+                && username.length() <= MAX_USERNAME_LENGTH
+                && username.matches("[a-zA-Z_0-9]*");
     }
 
     public static String pluralize(int count, String word) {
