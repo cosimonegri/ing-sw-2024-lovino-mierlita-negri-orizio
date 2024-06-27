bd45133d7cb5e6381b5c5f3cc4dc291f2b6e08d3
Davide Orizio <davide.orizio@mail.polimi.it>
2024-06-27 13:00:37 +0200

    - javadoc for MainController and GameController

--- src/main/java/it/polimi/ingsw/controller/GameController.java
+++ src/main/java/it/polimi/ingsw/controller/GameController.java
@@ -18,6 +18,9 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Control the action relative to a specific game
+ */
 public class GameController {
     private final int gameId;
     private final Game model;
@@ -31,14 +34,30 @@ public class GameController {
         return this.gameId;
     }
 
+    /**
+     * Send a message to the specified player
+     * @param username the player's username
+     * @param message the message to be sent
+     */
     synchronized public void notifyListener(String username, ServerToClientMessage message) {
         model.notifyListener(username, message);
     }
 
+    /**
+     * Send a message to all players in the game lobby
+     * @param message the message to be sent
+     */
     synchronized public void notifyAllListeners(ServerToClientMessage message) {
         model.notifyAllListeners(message);
     }
 
+    /**
+     * Add a new player to the game and
+     * start the game if the lobby is full
+     * @param username the player to be added
+     * @param listener the player's listener
+     * @throws LobbyFullException when the lobby is already full
+     */
     synchronized public void addPlayer(String username, GameListener listener) throws LobbyFullException {
         model.addPlayer(username, listener);
         if (model.isLobbyFull()) {
@@ -47,6 +66,11 @@ public class GameController {
         }
     }
 
+    /**
+     * Remove a player from the lobby and delete the game if
+     * it's already started.
+     * @param username the player to be removed
+     */
     synchronized public void removePlayer(String username) {
         if (model.getGamePhase() == GamePhase.WAITING) {
             model.removePlayer(username);
@@ -55,6 +79,13 @@ public class GameController {
         }
     }
 
+    /**
+     * Assign the requested marker to the player
+     * @param username the player's username
+     * @param marker the marker to be assigned
+     * @throws MarkerNotValidException if the marker is already taken by another player
+     * @throws ActionNotValidException if the request is invalid
+     */
     synchronized public void chooseMarker(String username, Marker marker) throws MarkerNotValidException, ActionNotValidException {
         Optional<Player> player = model.getPlayer(username);
         if (model.getGamePhase() != GamePhase.STARTER || player.isEmpty() || hasChosenMarker(player.get())) {
@@ -73,6 +104,12 @@ public class GameController {
         }
     }
 
+    /**
+     * Play the starter card
+     * @param username the player's username
+     * @param flipped how the starter card is to be played
+     * @throws ActionNotValidException if the request is invalid
+     */
     synchronized public void playStarter(String username, boolean flipped) throws ActionNotValidException {
         Optional<Player> player = model.getPlayer(username);
         if (model.getGamePhase() != GamePhase.STARTER || player.isEmpty() || hasPlayedStarter(player.get())) {
@@ -88,6 +125,13 @@ public class GameController {
         }
     }
 
+    /**
+     * Choose the personal objective
+     * @param username the player
+     * @param objective the card chosen
+     * @throws CardNotInHandException if the card was not a player's option
+     * @throws ActionNotValidException if the request is invalid
+     */
     synchronized public void chooseObjective(String username, ObjectiveCard objective) throws CardNotInHandException, ActionNotValidException {
         Optional<Player> player = model.getPlayer(username);
         if (model.getGamePhase() != GamePhase.OBJECTIVE || player.isEmpty() || hasChosenObjective(player.get())) {
@@ -104,6 +148,17 @@ public class GameController {
         }
     }
 
+    /**
+     * Play a card during the game
+     * @param username the player
+     * @param card the card to be played
+     * @param flipped how the card is to be played
+     * @param coords where the card is to be player
+     * @throws CardNotInHandException when the card was not in the player's hand
+     * @throws CoordinatesNotValidException when the coordinates are not valid
+     * @throws NotEnoughResourcesException when the player's resources are not enough for the card to be played
+     * @throws ActionNotValidException if the request is invalid
+     */
     synchronized public void playCard(String username, PlayableCard card, boolean flipped, Coordinates coords)
             throws CardNotInHandException, CoordinatesNotValidException, NotEnoughResourcesException, ActionNotValidException
     {
@@ -133,6 +188,15 @@ public class GameController {
         }
     }
 
+    /**
+     * Draw a card from a deck or from the board
+     * @param username the player
+     * @param type the deck from which to draw
+     * @param card the card to take from the board
+     * @throws EmptyDeckException it the requested deck is empty
+     * @throws CardNotOnBoardException it the requested card is not on the board
+     * @throws ActionNotValidException if the request is invalid
+     */
     synchronized public void drawCard(String username, DrawType type, PlayableCard card)
             throws EmptyDeckException, CardNotOnBoardException, ActionNotValidException
     {
--- src/main/java/it/polimi/ingsw/controller/MainController.java
+++ src/main/java/it/polimi/ingsw/controller/MainController.java
@@ -8,6 +8,9 @@ import it.polimi.ingsw.utilities.Config;
 
 import java.util.*;
 
+/**
+ * Control the creation and deletion of new games
+ */
 public class MainController {
     private static MainController instance;
     private final Map<Integer, GameController> games;
@@ -26,6 +29,13 @@ public class MainController {
         return instance;
     }
 
+    /**
+     * Add the new player to the map of listeners
+     * @param username the new player's username
+     * @param listener the new player's game listener
+     * @throws UsernameNotValidException when the username is not valid
+     * @throws UsernameAlreadyTakenException when a player with this username already exists
+     */
     synchronized public void connect(String username, GameListener listener) throws UsernameNotValidException, UsernameAlreadyTakenException {
         if (!Config.isUsernameValid(username)) {
             throw new UsernameNotValidException();
@@ -36,12 +46,25 @@ public class MainController {
         this.usernameToListener.put(username, listener);
     }
 
+    /**
+     * Send a message to the specified client
+     * @param username the username of the player to be notified
+     * @param message the message to send
+     */
     synchronized public void notifyListener(String username, ServerToClientMessage message) {
         if (this.isUsernameConnected(username)) {
             usernameToListener.get(username).updateFromModel(message);
         }
     }
 
+    /**
+     * Create a new game
+     * @param username the player requesting the new game
+     * @param playersCount the number of players in the game
+     * @return a controller for the new game
+     * @throws PlayersCountNotValidException when the players count is not valid
+     * @throws CannotCreateGameException when the game controller can't be created
+     */
     synchronized public GameController createGame(String username, int playersCount)
             throws PlayersCountNotValidException, CannotCreateGameException
     {
@@ -61,6 +84,14 @@ public class MainController {
         }
     }
 
+    /**
+     * Add a player to an existing game
+     * @param username the player to be joined
+     * @param gameId the game to join
+     * @return the game controller for the game joined
+     * @throws LobbyNotValidException if the id is not valid
+     * @throws LobbyFullException if the game lobby is full
+     */
     synchronized public GameController joinGame(String username, int gameId) throws LobbyNotValidException, LobbyFullException {
         if (!this.isUsernameConnected(username)) {
             //todo maybe handle this case
@@ -75,6 +106,12 @@ public class MainController {
         return game;
     }
 
+    /**
+     * Remove a player from a lobby/game
+     * @param username the player to be removed
+     * @return the game controller from which the player is removed
+     * @throws UsernameNotPlayingException when the player is not in a game/lobby
+     */
     synchronized public GameController leaveGame(String username) throws UsernameNotPlayingException {
         if (!this.isUsernameConnected(username)) {
             throw new UsernameNotPlayingException(username);
@@ -88,7 +125,6 @@ public class MainController {
         }
         return game;
     }
-
     synchronized public GameController getGameOfPlayer(String username) throws UsernameNotPlayingException {
         for (int gameId : this.games.keySet()) {
             GameController game = this.games.get(gameId);
@@ -108,6 +144,10 @@ public class MainController {
         return this.usernameToListener.containsKey(username);
     }
 
+    /**
+     * Create a new random id for a game
+     * @return the new id
+     */
     private int generateGameId() {
         Random random = new Random();
         int generatedId;
